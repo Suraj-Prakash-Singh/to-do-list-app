c052e57a74be0b2693556ab778cfb9b968b0505b
TesnSai <shivamchaudhary8476@gmail.com>
2022-04-14 15:00:45 +0530

    Added task deletion and addition functionality

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-var delbtns = document.querySelectorAll('#list .delete');
+const list = document.querySelector('#list ul');
 
-//turn ainto array
-delbtns.forEach(function(btn){
-    btn.addEventListener('click',function(e){
+//delete tasks
+list.addEventListener('click',function(e){
+    if(e.target.className == 'delete'){
         const li = e.target.parentElement;
-
-        li.parentElement.removeChild(li);
-    })
+        // li.parentElement.removeChild(li);
+        //or
+        list.removeChild(li);
+    }
 })
 
-//we can also prebvent the default behaviour of events
-//a is a link to another website we want to stop to go towards it on clicking the link 
 
-// const link = document.querySelector('#banner a');
+const addForm = document.forms['add-task'];
 
-// link.addEventListener('click',function(e){
-//     e.preventDefault();
-//     console.log('navigation to',e.target.textContent,'Was prevented');
-// })
+addForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+
+    //mY way-> clone a li and edit the text
+    // const l = document.querySelector('#list ul li');
+    // if (typeof value !== 'undefined' && value) {
+    //     //deal with value'
+    //     const clonedl = l.cloneNode(true);
+    //     clonedl.querySelector('.name').innerHTML = value;
+    //     l.parentNode.appendChild(clonedl);
+    // }
+
+    //net ninja way
+
+    //1. Create elements
+    const l = document.createElement('li');
+    const taskname = document.createElement('span');
+    const delbtn = document.createElement('span');
+
+    //2. Add content to elements
+    taskname.textContent = value;
+    delbtn.textContent = 'Delete';
+
+    //3. Append elements to the DOM
+    l.appendChild(taskname);
+    l.appendChild(delbtn);
+    list.appendChild(l);
+
+})
+
+    
